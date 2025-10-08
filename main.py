d64ade12d095f8d51fa8b7055df9284bde64c508
PeterFrances <peter.romany2000@icloud.com>
2025-10-08 11:20:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import streamlit as st
 
-def login_screen():
+
+if not st.user.is_logged_in:
     st.header("This app is private.")
     st.subheader("Please log in.")
     st.button("Log in with Google", on_click=st.login)
-
-if not st.user.is_logged_in:
-    login_screen()
 else:
     st.header(f"Welcome, {st.user.name}!")
     st.button("Log out", on_click=st.logout)
