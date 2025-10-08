bcbaf4e7abb46530cac4443a72684d4795eb101d
PeterFrances <peter.romany2000@icloud.com>
2025-10-08 17:47:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 st.user
-if not st.user.is_logged_in:
-    if st.button("Log in"):
-        s=st.login()
-else:
-    if st.button("Log out"):
-        st.logout()
-    st.write(f"Hello, {st.user.name}!")
+if st.button("Log in"):
+    st.login('google')
+# if not st.user.is_logged_in:
+#     if st.button("Log in"):
+#         st.login('google')
+# else:
+#     if st.button("Log out"):
+#         st.logout()
+#     st.write(f"Hello, {st.user.name}!")
