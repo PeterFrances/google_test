373834c81bab46a4ada0d266a6bf354f7718aebb
PeterFrances <peter.romany2000@icloud.com>
2025-10-08 17:25:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 import streamlit as st
-import pandas as pd
 
-st.set_page_config(layout='wide', initial_sidebar_state="collapsed")
-st.write('hello')
-
-def login_screen():
-    st.header("This app is private.")
-    st.subheader("Please log in.")
-    if st.button('press'):
-        st.login('google')
-    if not st.user.is_logged_in:
-        st.write(st.user)
-    # st.button("Log in with Google", on_click=st.login('google'))
-# if not st.user.is_logged_in:
-login_screen()
-# else:
-#     st.user
-#     st.header(f"Welcome, {st.user.name}!")
-#     st.button("Log out", on_click=st.logout)
+if not st.user.is_logged_in:
+    if st.button("Log in"):
+        st.login()
+else:
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.user.name}!")
