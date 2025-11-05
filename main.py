c582216a68b4b37c9b3a881deca46a071440c4d1
PeterFrances <peter.romany2000@icloud.com>
2025-11-05 16:09:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import streamlit as st
-if st.button('press'):
-    st.logout()
-if not st.user.is_logged_in:
-    if st.button("Log in"):
-        st.login('google')
-else:
-    if st.button("Log out"):
-        st.logout()
-    st.write(f"Hello, {st.user.name}!")
+
+# Get the user's email (only works in Streamlit Cloud with invited viewers)
+user_info = st.context.cookies
+user_email = st.context.headers.get("Cf-Access-Authenticated-User-Email", "")
+
+# For newer Streamlit versions (1.28+):
+try:
+    user_email = st.experimental_user.email
+except:
+    user_email = ""
+
+
+# import streamlit as st
+# if st.button('press'):
+#     st.logout()
+# if not st.user.is_logged_in:
+#     if st.button("Log in"):
+#         st.login('google')
+# else:
+#     if st.button("Log out"):
+#         st.logout()
+#     st.write(f"Hello, {st.user.name}!")
