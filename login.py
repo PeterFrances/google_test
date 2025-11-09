6fd87d6b58b7ae77d7633612cf000e90643455a5
PeterFrances <peter.romany2000@icloud.com>
2025-11-09 15:08:45 +0200

    Delete login.py

--- login.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Check if user is logged in
-if hasattr(st.user, 'is_logged_in') and st.user.is_logged_in:        
-    if st.button("Log out"):
-        st.logout()    
-else:    
-        st.login('google')  # Using named provider
