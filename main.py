dd83295673538b82e92e3d19c9019d4ef1cf0919
PeterFrances <peter.romany2000@icloud.com>
2025-11-09 13:04:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,6 @@ else:
     st.write("Please log in to continue")
     if st.button("Log in with Google"):
         st.login('google')  # Using named provider
-# import streamlit as st
-# if st.button('press'):
-#     st.logout()
-# if not st.user.is_logged_in:
-#     if st.button("Log in"):
-#         st.login('google')
-# else:
-#     if st.button("Log out"):
-#         st.logout()
-#     st.write(f"Hello, {st.user.name}!")
-# st.write(st.user)
-# st.write(f"Hello, {st.user.name}!")
+        
+st.write(st.secrets)
+st.secrets
